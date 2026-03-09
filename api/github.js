1f8db300e7dd74da71db30b44c8fc76cf171b98e
CoolSpidey89 <fireraider89@gmail.com>
2026-03-09 15:46:08 +0530

    fix api response types

--- api/github.js
+++ api/github.js
@@ -4,10 +4,17 @@ export default async function handler(req, res) {
   const response = await fetch(url, {
     headers: {
       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      Accept: "application/vnd.github+json",
-    },
+      Accept: "application/vnd.github+json"
+    }
   });
 
-  const text = await response.text();
-  res.status(200).send(text);
+  const contentType = response.headers.get("content-type");
+
+  if (contentType && contentType.includes("application/json")) {
+    const data = await response.json();
+    res.status(response.status).json(data);
+  } else {
+    const text = await response.text();
+    res.status(response.status).send(text);
+  }
 }
